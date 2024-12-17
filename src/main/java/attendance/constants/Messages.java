722b83b4670235e72869d15829971ce9a5932c0e
김건학 <kkh10002@naver.com>
2024-12-17 21:02:21 +0900

    feat(Messages): 출석 확인 메시지 상수 처리

---
+++ src/main/java/attendance/constants/Messages.java
@@ -0,0 +1,35 @@
+package attendance.constants;
+
+public enum Messages {
+    // input
+    INPUT_NICKNAME("닉네임을 입력해 주세요."),
+    INPUT_ATTENDANCE_TIME("등교 시간을 입력해 주세요."),
+
+    // output
+    TODAY_AND_FUNCTIONS("오늘은 %d월 %02d일 %s입니다. 기능을 선택해 주세요.\n"
+            + "1. 출석 확인\n"
+            + "2. 출석 수정\n"
+            + "3. 크루별 출석 기록 확인\n"
+            + "4. 제적 위험자 확인\n"
+            + "Q. 종료\n"),
+    CONFIRM_ATTENDANCE("%d월 %02d일 %s %s (%s)"),
+
+    // error
+    INVALID_INPUT_FORMAT("잘못된 형식을 입력하였습니다."),
+    NO_EXIST_NICKNAME("등록되지 않은 닉네임입니다."),
+    INVALID_AT_RED_DAY("%d월 %02d일 %s은 등교일이 아닙니다.");
+
+    private final String message;
+
+    Messages(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getErrorMessage() {
+        return "[ERROR] " + message;
+    }
+}
