eb0d4d5efe5933d38565c06a0fa56e196d8efa38
김건학 <kkh10002@naver.com>
2024-12-17 21:03:53 +0900

    feat(DateUtils): 날짜 시각 유틸 기능 추가
    오늘의 요일을 반환하는 기능 추가
    오늘의 월을 반환하는 기능 추가
    오늘의 일을 반환하는 기능 추가

---
+++ src/main/java/attendance/utils/DateUtils.java
@@ -0,0 +1,36 @@
+package attendance.utils;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public final class DateUtils {
+    private DateUtils() {
+    }
+
+    public static String getTodayDayOfWeek() {
+        LocalDateTime now = getLocalDateTime();
+        DayOfWeek dayOfWeek = now.getDayOfWeek();
+
+        return dayOfWeek.getDisplayName(TextStyle.FULL, Locale.KOREAN);
+    }
+
+    public static int getTodayMonth() {
+        LocalDateTime now = getLocalDateTime();
+        Month month = now.getMonth();
+
+        return month.getValue();
+    }
+
+    public static int getTodayDay() {
+        LocalDateTime now = getLocalDateTime();
+        return now.getDayOfMonth();
+    }
+
+    private static LocalDateTime getLocalDateTime() {
+        return DateTimes.now();
+    }
+}
