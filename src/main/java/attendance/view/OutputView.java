268b3075883b49359bf32501244a591dfb830111
김건학 <kkh10002@naver.com>
2024-12-17 21:25:35 +0900

    style(OutputView): 개행 추가

--- src/main/java/attendance/view/OutputView.java
+++ src/main/java/attendance/view/OutputView.java
@@ -5,5 +5,6 @@ import static attendance.constants.Messages.TODAY_AND_FUNCTIONS;
 public class OutputView {
     public void printTodayAndFunctions(final int month, final int day, final String dayOfWeek) {
         System.out.printf(TODAY_AND_FUNCTIONS.getMessage(), month, day, dayOfWeek);
+        System.out.println();
     }
 }
