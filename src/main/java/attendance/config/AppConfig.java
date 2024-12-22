959c2f32a255b19d2ec819ac44285be83837ba8f
김건학 <kkh10002@naver.com>
2024-12-22 14:18:31 +0900

    feat(Controller): 컨트롤러에 출석부 기능 등록
    프로그램을 실행하면 자동으로 파일을 읽어 출석부를 생성하도록 구현했습니다.

--- src/main/java/attendance/config/AppConfig.java
+++ src/main/java/attendance/config/AppConfig.java
@@ -1,20 +1,28 @@
 package attendance.config;
 
+import static attendance.constants.Messages.INVALID_FILE_PATH;
+
 import attendance.controller.Controller;
+import attendance.domain.RecordBook;
 import attendance.view.InputView;
 import attendance.view.OutputView;
+import java.io.FileNotFoundException;
 
 public class AppConfig {
+    private final static String FILE_PATH = "src/main/resources/attendances.csv";
+
     private final InputView inputView;
     private final OutputView outputView;
+    private final RecordBook recordBook;
 
     public AppConfig() {
         this.inputView = inputView();
         this.outputView = outputView();
+        this.recordBook = recordBook();
     }
 
     public Controller controller() {
-        return new Controller(inputView, outputView);
+        return new Controller(inputView, outputView, recordBook);
     }
 
     public InputView inputView() {
@@ -23,5 +31,14 @@ public class AppConfig {
 
     public OutputView outputView() {
         return new OutputView();
+    }
+
+    public RecordBook recordBook() {
+        try {
+            return RecordBook.from(FILE_PATH);
+        } catch (FileNotFoundException e) {
+            System.out.println(INVALID_FILE_PATH.getErrorMessage());
+        }
+        return null;
     }
 }
--- src/main/java/attendance/controller/Controller.java
+++ src/main/java/attendance/controller/Controller.java
@@ -4,6 +4,7 @@ import static attendance.utils.DateUtils.getTodayDay;
 import static attendance.utils.DateUtils.getTodayDayOfWeek;
 import static attendance.utils.DateUtils.getTodayMonth;
 
+import attendance.domain.RecordBook;
 import attendance.domain.RedDay;
 import attendance.view.InputView;
 import attendance.view.OutputView;
@@ -12,10 +13,12 @@ import java.util.Objects;
 public class Controller {
     private final InputView inputView;
     private final OutputView outputView;
+    private final RecordBook recordBook;
 
-    public Controller(final InputView inputView, final OutputView outputView) {
+    public Controller(final InputView inputView, final OutputView outputView, final RecordBook recordBook) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.recordBook = recordBook;
     }
 
     public void run() {
